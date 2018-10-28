bd081a357d5e072075c980c69b588ce638128b01
Anna Kutarba <anna.kutarba@jetbrains.com>
2018-10-28 18:14:15 +0100

    correct time zone

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Testguru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Munich'
+    config.time_zone = 'Moscow'
     config.default_locale = :en
 
     # Settings in config/environments/* take precedence over those specified here.
