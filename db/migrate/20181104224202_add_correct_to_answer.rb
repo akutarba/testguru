abafbd462026faaf8612b39eb97b7bc140ea89c3
Anna Kutarba <anna.kutarba@jetbrains.com>
2018-11-08 23:53:08 +0100

    added default value to answers(from previous review)

--- db/migrate/20181104224202_add_correct_to_answer.rb
+++ db/migrate/20181104224202_add_correct_to_answer.rb
@@ -1,5 +1,5 @@
 class AddCorrectToAnswer < ActiveRecord::Migration[5.2]
   def change
-    add_column(:answers, :correct, :boolean)
+    add_column(:answers, :correct, :boolean, default: false)
   end
 end
