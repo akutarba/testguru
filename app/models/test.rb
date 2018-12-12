4966509690898246646e34226ef9af7b122bc90e
Anna Kutarba <anna.kutarba@jetbrains.com>
2018-12-12 20:31:49 +0100

    added associations and test_passages

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,7 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  has_many :questions
 
   def self.get_titles(category_name)
     Test.joins(' JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category_name)
